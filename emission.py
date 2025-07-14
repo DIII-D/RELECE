7c029e6392e21748095abd5f15053e7764173d9b
Isaac Goodman <isaacdg@stanford.edu>
2025-07-14 11:54:56 -0700

    Write refraction function

--- emission.py
+++ emission.py
@@ -26,3 +26,33 @@ wpe = np.sqrt(ne * constants.e**2
               / (constants.m_e * constants.epsilon_0))  # Plasma freq (rad/s)
 wce = 2*np.pi * fce
 wr = wce / 2 * (1 + np.sqrt(1 + 4 * wpe**2/wce**2))  # RH cutoff (rad/s)
+
+
+def refraction(w=w, wpe=wpe, wce=wce, x_mode=x_mode):
+    """Return the cold plasma refraction index squared.
+
+    This index depends on the wave mode. The O mode has the same index
+    as in the isotropic case, whereas the X mode is subject to the
+    effects of the ambient magnetic field.
+
+    Reference: R. Parker, “Electromagnetic waves in plasmas,” in
+    Introduction to Plasma Physics I, MIT OpenCourseWare, 2006,
+    pp. 96–144
+
+    Keyword arguments:
+    w -- the wave frequency (default: global)
+    wpe -- the plasma frequency (default: global)
+    wce -- the cyclotron frequency (default: global)
+    x_mode -- whether the x_mode is selected (default: global)
+    """
+
+    if (x_mode):
+        R = 1 - wpe**2 / (w * (w+wce))
+        L = 1 - wpe**2 / (w * (w-wce))
+        S = 0.5 * (R + L)
+
+        N2 = R * L / S
+    else:
+        N2 = 1 - wpe**2 / w**2
+
+    return N2
