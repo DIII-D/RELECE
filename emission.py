a86d2ca568a295949636f2b2482bd41a86322184
Isaac Goodman <isaacdg@stanford.edu>
2025-07-15 12:00:55 -0700

    Write hermitian_dispersion function

--- emission.py
+++ emission.py
@@ -33,7 +33,7 @@ c = constants.speed_of_light * 1e2   # cm/s
 m_e = constants.electron_mass * 1e3  # g
 
 
-def _calculate_refraction_coefs(w=w, wpe=wpe, wce=wce, theta=theta):
+def _calculate_refraction_coefs(w, wpe, wce, theta, tensor=False):
     """Return coefficients needed to determine refraction index."""
     P = 1 - wpe**2 / w**2
     R = 1 - wpe**2 / (w * (w+wce))
@@ -43,7 +43,8 @@ def _calculate_refraction_coefs(w=w, wpe=wpe, wce=wce, theta=theta):
     A = S * np.sin(theta)**2 + P * np.cos(theta)**2
     B = R * L * np.sin(theta)**2 + P * S * (1 + np.cos(theta)**2)
     F = (R*L - P*S)**2 * np.sin(theta)**4 + 4 * P**2 * D**2 * np.cos(theta)**2
-
+    if (tensor):
+        return S, D, P
     return A, B, F
 
 
@@ -54,7 +55,7 @@ def _refraction_modes(A, B, F):
     return N_plus, N_minus
 
 
-def refraction(w=w, wpe=wpe, wce=wce, theta=theta, x_mode=x_mode):
+def refraction(w, wpe, wce, theta, x_mode=False):
     """Calculates the cold plasma refraction index squared.
 
     This index depends on the wave mode. The O mode is cut off (i.e.
@@ -79,12 +80,13 @@ def refraction(w=w, wpe=wpe, wce=wce, theta=theta, x_mode=x_mode):
     :returns: cold plasma refraction index squared
     :rtype: float
     """
-    A, B, F = _calculate_refraction_coefs(w, wpe, wce)
-    A_cutoff, B_cutoff, F_cutoff = _calculate_refraction_coefs(wpe, wpe, wce)
+    A, B, F = _calculate_refraction_coefs(w, wpe, wce, theta)
+    A_cutoff, B_cutoff, F_cutoff = _calculate_refraction_coefs(wpe, wpe, wce, theta)
     N2_plus, N2_minus = _refraction_modes(A, B, F)
-    N2_plus_cutoff, N2_minus_cutoff = _refraction_modes(A_cutoff, B_cutoff,
+    N2_plus_cutoff, _ = _refraction_modes(A_cutoff, B_cutoff,
                                                         F_cutoff)
 
+    # Select the wpe cutoff for the O mode
     if (N2_plus_cutoff < tolerance):
         N2_O = N2_plus
         N2_X = N2_minus
@@ -97,7 +99,45 @@ def refraction(w=w, wpe=wpe, wce=wce, theta=theta, x_mode=x_mode):
     return N2_O
 
 
-def eps_h():
-    """Return cold plasma Hermitian dielectric tensor.
+def hermitian_dispersion(w, wpe, wce, theta, x_mode=False):
+    """Calculates equation 5.75 from the reference.
+
+    The determinant of this tensor yields a quadratic expression for
+    the cold plasma dispersion relation. It can also be used to
+    determine the Stix frame as well as the hermitian dielectric
+    tensor.
+
+    Reference: R. Parker, “Electromagnetic waves in plasmas,” in
+    Introduction to Plasma Physics I, MIT OpenCourseWare, 2006,
+    pp. 96-144
+
+    :param w: wave frequency
+    :type w: float
+    :param wpe: plasma frequency
+    :type wpe: float
+    :param wce: cyclotron frequency
+    :type wce: float
+    :param theta: propagation angle
+    :type theta: float
+    :param x_mode: whether the x_mode is selected
+    :type x_mode: bool
+    :returns: cold plasma dispersion tensor
+    :rtype: np.ndarray
     """
-    pass
+    S, D, P = _calculate_refraction_coefs(w, wpe, wce, tensor=True)
+    N2 = refraction(w, wpe, wce, theta, x_mode)
+
+    D00 = -N2 * np.cos(theta)**2 + S
+    D01 = -1j * D
+    D02 = N2 * np.sin(theta) * np.cos(theta)
+    D10 = 1j * D
+    D11 = -N2 + S
+    D12 = 0
+    D20 = N2 * np.sin(theta) * np.cos(theta)
+    D21 = 0
+    D22 = -N2 * np.sin(theta)**2 + P
+
+    Dij = np.array([[D00, D01, D02],
+                    [D10, D11, D12],
+                    [D20, D21, D22]])
+    return Dij
