c0880907ef682c24dca55052ef222d798cc20451
Isaac Goodman <isaacdg@stanford.edu>
2025-07-14 19:47:05 -0700

    Fix refraction function

--- emission.py
+++ emission.py
@@ -10,7 +10,7 @@ from scipy import constants, special
 import matplotlib.pyplot as plt
 
 
-# Experimental parameters
+# Parameters
 f = 90e9                    # EM wave frequency (Hz)
 ne = 2e19                   # Electron number density (per m^3)
 fce = 50e9                  # Cyclotron frequency (Hz)
@@ -19,6 +19,7 @@ n = 2                       # ECE harmonic
 x_mode = True               # X or O mode
 theta = np.pi / 2           # Viewing angle
 res_ellipse_N = 1000        # Number of points on resonant ellipse
+tolerance = 0.001
 
 # Generated values
 w = 2*np.pi * f
@@ -27,32 +28,76 @@ wpe = np.sqrt(ne * constants.e**2
 wce = 2*np.pi * fce
 wr = wce / 2 * (1 + np.sqrt(1 + 4 * wpe**2/wce**2))  # RH cutoff (rad/s)
 
+# Constants
+c = constants.speed_of_light * 1e2   # cm/s
+m_e = constants.electron_mass * 1e3  # g
 
-def refraction(w=w, wpe=wpe, wce=wce, x_mode=x_mode):
-    """Return the cold plasma refraction index squared.
 
-    This index depends on the wave mode. The O mode has the same index
-    as in the isotropic case, whereas the X mode is subject to the
-    effects of the ambient magnetic field.
+def _calculate_refraction_coefs(w=w, wpe=wpe, wce=wce, theta=theta):
+    """Return coefficients needed to determine refraction index."""
+    P = 1 - wpe**2 / w**2
+    R = 1 - wpe**2 / (w * (w+wce))
+    L = 1 - wpe**2 / (w * (w-wce))
+    S = (R + L) / 2
+    D = (R - L) / 2
+    A = S * np.sin(theta)**2 + P * np.cos(theta)**2
+    B = R * L * np.sin(theta)**2 + P * S * (1 + np.cos(theta)**2)
+    F = (R*L - P*S)**2 * np.sin(theta)**4 + 4 * P**2 * D**2 * np.cos(theta)**2
+
+    return A, B, F
+
+
+def _refraction_modes(A, B, F):
+    """Return the refraction indices for both modes."""
+    N_plus = (B + F) / (2 * A)
+    N_minus = (B - F) / (2 * A)
+    return N_plus, N_minus
+
+
+def refraction(w=w, wpe=wpe, wce=wce, theta=theta, x_mode=x_mode):
+    """Calculates the cold plasma refraction index squared.
+
+    This index depends on the wave mode. The O mode is cut off (i.e.
+    N^2 <= 0) at the plasma frequency,  whereas the X mode still
+    propagates. This is the physical difference between the modes which
+    we shall use to select the correct one.
 
     Reference: R. Parker, “Electromagnetic waves in plasmas,” in
     Introduction to Plasma Physics I, MIT OpenCourseWare, 2006,
-    pp. 96–144
+    pp. 96-144
 
-    Keyword arguments:
-    w -- the wave frequency (default: global)
-    wpe -- the plasma frequency (default: global)
-    wce -- the cyclotron frequency (default: global)
-    x_mode -- whether the x_mode is selected (default: global)
+    :param w: wave frequency
+    :type w: float
+    :param wpe: plasma frequency
+    :type wpe: float
+    :param wce: cyclotron frequency
+    :type wce: float
+    :param theta: propagation angle
+    :type theta: float
+    :param x_mode: whether the x_mode is selected
+    :type x_mode: bool
+    :returns: cold plasma refraction index squared
+    :rtype: float
     """
+    A, B, F = _calculate_refraction_coefs(w, wpe, wce)
+    A_cutoff, B_cutoff, F_cutoff = _calculate_refraction_coefs(wpe, wpe, wce)
+    N2_plus, N2_minus = _refraction_modes(A, B, F)
+    N2_plus_cutoff, N2_minus_cutoff = _refraction_modes(A_cutoff, B_cutoff,
+                                                        F_cutoff)
+
+    if (N2_plus_cutoff < tolerance):
+        N2_O = N2_plus
+        N2_X = N2_minus
+    else:
+        N2_O = N2_minus
+        N2_X = N2_plus
 
     if (x_mode):
-        R = 1 - wpe**2 / (w * (w+wce))
-        L = 1 - wpe**2 / (w * (w-wce))
-        S = 0.5 * (R + L)
+        return N2_X
+    return N2_O
 
-        N2 = R * L / S
-    else:
-        N2 = 1 - wpe**2 / w**2
 
-    return N2
+def eps_h():
+    """Return cold plasma Hermitian dielectric tensor.
+    """
+    pass
