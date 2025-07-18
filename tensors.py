a67dbb6fe5d7f6a495471a0e6474cfe17f875d61
Isaac Goodman <isaacdg@stanford.edu>
2025-07-18 10:05:50 -0700

    More eps_a

--- tensors.py
+++ tensors.py
@@ -1,3 +1,6 @@
+"""
+This code calculates the tensors needed to perform ECE
+calculations."""
 import numpy as np
 from scipy import constants, special
 
@@ -18,23 +21,90 @@ def _get_eps_h_coefs(w, wpe, wce, n_par, Te):
         gamma_s[i] = np.sqrt(1 + ps[i]**2)
         F = np.exp(-mu * (gamma_s[i] - 1))
 
-    return a, R, s, xi, F, ps, q
+    return mu, a, R, s, xi, F, ps, q
 
 
-def _aij(w, wpe, wce, n_par, Te):
-    a, R, s, xi, F, ps, q = _get_eps_h_coefs(w, wpe, wce, n_par, Te)
+def _aij(w, wce, a, R, s, xi, F, ps, q):
     sum_11 = sum_13 = sum_33 = 0
     for i in range(2):
         sum_11 += (1 + s[i] / xi) * F[i]
         sum_13 += (ps[i] + s[i] * (ps[i] + 2*s[i]*q) / xi
                    + 3*s[i]*q / xi**2) * F[i]
-        sum_33 += (ps[i]**2 + s[i]*ps[i] * (ps[i] + 4*s[i]*q) / xi
-                   + 6*s[i]*q * (ps[i] + s[i]*q) / xi**2
+        sum_33 += (ps[i]**2 + s[i]*ps[i]*(ps[i] + 4*s[i]*q) / xi
+                   + 6*s[i]*q*(ps[i] + s[i]*q) / xi**2
                    + 12*s[i]*q**2 / xi**3) * F[i]
     a11 = a * R * sum_11
     a13 = a * R * (w/wce) * sum_13
     a33 = a * R * (w/wce)**2 * sum_33
+    return a11, a13, a33
 
 
-def eps_a():
-    pass
+def _bij(w, wce, n_par, mu, a, R, s, xi, F, ps, q):
+    sum_11 = sum_13 = sum_33 = 0
+    for i in range(2):
+        sum_11 += s[i] * (1 + 3*s[i] / xi + 3 / xi**2) * F[i]
+        sum_13 += s[i] * (ps[i] + 3*s[i]*(ps[i] + s[i]*q) / xi
+                          + 3*(ps[i] + 4*s[i]*q) / xi**2
+                          + 15*q / xi**3) * F[i]
+        sum_33 += s[i] * (ps[i]**2 + 3*s[i]*ps[i]*(ps[i] + 2*s[i]*q) / xi
+                          + 3*(ps[i]**2 + 8*s[i]*q*ps[i] + 4*q**2) / xi**2
+                          + 30*q*(ps[i] + 2*s[i]*q) / xi**3
+                          + 90*q**2 / xi**4) * F[i]
+    b11 = a * R * (R / (n_par * mu)) * (w/wce)**2 * sum_11
+    b13 = a * R * (R / (n_par * mu)) * (w/wce)**3 * sum_13
+    b33 = a * R * (R / (n_par * mu)) * (w/wce)**4 * sum_33
+    return b11, b13, b33
+
+
+def _cij(w, wce, n_par, mu, a, R, s, xi, F, ps, q):
+    return -4 * _bij(w, 2*wce, n_par, mu, a, R, s, xi, F, ps, q)
+
+
+def _dij(w, wce, n_par, mu, a, R, s, xi, F, ps, q):
+    sum_11 = sum_13 = sum_33 = 0
+    for i in range(2):
+        sum_11 += (1 + 6*s[i] / xi + 15 / xi**2 + 15*s[i] / xi**3) * F[s]
+        sum_13 += (ps[i] * (1 + 6*s[i] / xi + 15 / xi**2 + 15*s / xi**3)
+                  + 2 * (q / xi) * (2 + 15*s[i] / xi + 45 / xi**2
+                                  + 105*s[i] / (2*xi**3))) * F[i]
+        sum_33 += (ps[i]**2 * (1 + 6*s[i] / xi + 15 / xi**2 + 15*s[i] / xi**3)
+                  + 4 * (q*ps[i] / xi) * (2 + 15*s / xi + 45 / xi**2
+                                        + 105*s[i] / (2*xi**3))
+                  + 10 * (q / xi)**2 * (2 + 18*s[i] / xi + 63 / xi**2
+                                        + 84*s / xi**3)) * F[i]
+    d11 = a * R * (R / (n_par * mu))**2 * (5/8) * (w/wce)**4 * sum_11
+    d13 = a * R * (R / (n_par * mu))**2 * (5/8) * (w/wce)**5 * sum_13
+    d33 = a * R * (R / (n_par * mu))**2 * (5/8) * (w/wce)**6 * sum_33
+    return d11, d13, d33
+
+
+def _fij(w, wce, n_par, mu, a, R, s, xi, F, ps, q):
+    return -(128/5) * _dij(w, 2*wce, n_par, mu, a, R, s, xi, F, ps, q)
+
+
+def _gij(w, wce, n_par, mu, a, R, s, xi, F, ps, q):
+    return (243/5) * _dij(w, 3*wce, n_par, mu, a, R, s, xi, F, ps, q)
+
+
+def eps_a(w, wpe, wce, n_par, n_perp, Te):
+    mu, a, R, s, xi, F, ps, q = _get_eps_h_coefs(w, wpe, wce, n_par, Te)
+    if (np.real(R) <= 0):
+        return np.zeros((3, 3))  # Accounts for the 'S' in [1]
+    a11, a13, a33 = _aij(w, wce, a, R, s, xi, F, ps, q)
+    b11, b13, b33 = _bij(w, wce, n_par, mu, a, R, s, xi, F, ps, q)
+    c11, c13, c33 = _cij(w, wce, n_par, mu, a, R, s, xi, F, ps, q)
+    d11, d13, d33 = _dij(w, wce, n_par, mu, a, R, s, xi, F, ps, q)
+    f11, f13, f33 = _fij(w, wce, n_par, mu, a, R, s, xi, F, ps, q)
+    g11, g13, g33 = _gij(w, wce, n_par, mu, a, R, s, xi, F, ps, q)
+
+    e11 = a11 + n_perp**2 * (b11 + c11) + n_perp**4 * (d11 + f11 + g11)
+    e12 = -1j * (a11 + n_perp**2 * (2*b11 + c11)
+                 + n_perp**4 * (3*d11 + 3*f11/2 + g11))
+    e22 = (a11 + n_perp**2 * (3*b11 + c11)
+           + n_perp**4 * (37*d11/5 + 2*f11 + g11))
+    e13 = n_perp * (a13 + n_perp**2 * (b13 + c13)
+                    + n_perp**4 * (d13 + f13 + g13))
+    e23 = 1j * n_perp * (a13 + n_perp**2 * (2*b13 + c13)
+                         + n_perp**4 * (3*d13 + 3*f13/2 + g13))
+    e33 = n_perp**2 * (a33 + n_perp**2 * (b33 + c33)
+                       + n_perp**4 * (d**3))
